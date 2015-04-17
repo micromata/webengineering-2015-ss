61940a59c55dcef9f4dc6e52b91d82f23d4dcf18
Michael Lesniak <mlesniak@micromata.de>
2015-04-17 14:58:59 +0200

    ModelAndView.

--- webengineering/src/main/java/com/micromata/webengineering/MainController.java
+++ webengineering/src/main/java/com/micromata/webengineering/MainController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Main controller.
@@ -15,8 +16,11 @@ public class MainController {
   private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
 
   @RequestMapping("/")
-  public String index() {
+  public ModelAndView index() {
     LOG.info("Request to /index");
-    return "index";
+
+    ModelAndView mav = new ModelAndView("index");
+
+    return mav;
   }
 }
