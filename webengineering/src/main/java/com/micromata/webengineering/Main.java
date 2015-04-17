3d34107d5583f75a44aa62d60142b6aa6387ffa3
Michael Lesniak <mlesniak@micromata.de>
2015-04-17 14:37:35 +0200

    Date returned.

--- webengineering/src/main/java/com/micromata/webengineering/Main.java
+++ webengineering/src/main/java/com/micromata/webengineering/Main.java
@@ -2,13 +2,25 @@ package com.micromata.webengineering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@Controller
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
+  }
+
+  @ResponseBody
+  @RequestMapping("/")
+  public String index() {
+    return new Date().toString();
   }
 }
