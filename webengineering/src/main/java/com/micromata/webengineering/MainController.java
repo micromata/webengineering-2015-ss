43bf46848ff1e9e683ce222be0bb72c5d37c3b74
Michael Lesniak <mlesniak@micromata.de>
2015-04-17 14:44:15 +0200

    HTML templates.

--- webengineering/src/main/java/com/micromata/webengineering/MainController.java
+++ webengineering/src/main/java/com/micromata/webengineering/MainController.java
@@ -2,9 +2,6 @@ package com.micromata.webengineering;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Date;
 
 /**
  * Main controller.
@@ -13,9 +10,8 @@ import java.util.Date;
  */
 @Controller
 public class MainController {
-  @ResponseBody
   @RequestMapping("/")
   public String index() {
-    return new Date().toString();
+    return "index";
   }
 }
