73ffaab45ddffc8ba417253e61c3aa51288e903a
Michael Lesniak <mlesniak@micromata.de>
2015-04-17 14:35:07 +0200

    Error page ;-).

--- webengineering/src/main/java/com/micromata/webengineering/Main.java
+++ webengineering/src/main/java/com/micromata/webengineering/Main.java
@@ -1,10 +1,14 @@
 package com.micromata.webengineering;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  */
+@SpringBootApplication
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    SpringApplication.run(Main.class, args);
   }
 }
